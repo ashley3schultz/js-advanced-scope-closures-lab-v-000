5f2a1e125bb1d7b10b8a342a211ec857b459f3cb
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-30 20:52:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function produceDrivingRange(blockRange){
+function produceDrivingRange(rng){
   return function(beg, end){
-    return b = parseInt(beg)
+    let b = parseInt(beg)
+    let e = parseInt(end)
+    return (e - b) > rng ? `${e - b} blocks out of range` : `within range by ${e - b}`
+  }
+
+  eightBlockRange('10th', '20th')
 }
 
 class ProduceTipCalculator{
