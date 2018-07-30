ec8e9ce2ff5060642f36cdba9dcb65be9ffafea6
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-30 20:51:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function produceDrivingRange(blockRange){
   return function(beg, end){
-    let b = parseInt(beg)
-    let e = parseInt(end)
-    return e - b
+    return b = parseInt(beg)
 }
 
 class ProduceTipCalculator{
