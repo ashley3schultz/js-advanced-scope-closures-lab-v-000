dc50fe3e3cec72fa08ce3dd694fb3e7898117ece
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-30 20:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(rng){
   return function(beg, end){
     let b = parseInt(beg)
     let e = parseInt(end)
-    return (e - b) > rng ? `${e - b} blocks out of range` : `within range by ${e - b}`
+    return (e - b) > rng ? `${(e - b)-rng} blocks out of range` : `within range by ${e - b}`
   }
 
   eightBlockRange('10th', '20th')
