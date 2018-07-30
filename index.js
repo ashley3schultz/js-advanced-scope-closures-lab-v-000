09ac47e5f87936b4d6c2ba418b6cb307798c2d24
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-30 21:07:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function produceDrivingRange(rng){
+  return function(beg, end){
+    let b = parseInt(beg)
+    let e = parseInt(end)
+    return (e - b) > rng ? `${(e - b)-rng} blocks out of range` : `within range by ${rng-(e - b)}`
+  }
+}
+
+
+function produceTipCalculator(percent){
+  return function(total){
+    return total * percent
+  }
+}
